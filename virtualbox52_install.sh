52f0852d2e43addf47d0484ed7203f252f071d09
Rodrigo Fioravante <kbsafioravante@gmail.com>
2017-12-08 16:34:51 -0300

    Fixed Extension Packs Licence problem and now removing previous instalation of extension packs, this fix a script crash in case of a previous VirtualBox with Extensions Pack installed.

--- virtualbox52_install.sh
+++ virtualbox52_install.sh
@@ -17,7 +17,8 @@ wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-
 echo "deb http://download.virtualbox.org/virtualbox/debian $distro_codename contrib" > /etc/apt/sources.list.d/virtualbox.list
 apt update
 apt install -y virtualbox-5.2
+VBoxManage extpack uninstall "Oracle VM VirtualBox Extension Pack"
 wget -c http://download.virtualbox.org/virtualbox/5.2.2/Oracle_VM_VirtualBox_Extension_Pack-5.2.2-119230.vbox-extpack
-VBoxManage extpack install Oracle_VM_VirtualBox_Extension_Pack-5.2.2-119230.vbox-extpack --accept-license=b674970f720eb020ad18926a9268607089cc1703908696d24a04aa870f34c8e8
+VBoxManage extpack install Oracle_VM_VirtualBox_Extension_Pack-5.2.2-119230.vbox-extpack --accept-license=56be48f923303c8cababb0bb4c478284b688ed23f16d775d729b89a2e8e5f9eb
 rm Oracle_VM_VirtualBox_Extension_Pack-5.2.2-119230.vbox-extpack
 echo "### Fim da instalação do VirtualBox 5.2 ###"
