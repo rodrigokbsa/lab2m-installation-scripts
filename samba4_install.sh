a23e615a91a131d5c02a5756774cad1fa0363416
Rodrigo Fioravante <kbsafioravante@gmail.com>
2018-03-16 11:25:02 -0400

    Samba 4.8.0 download and compilation

--- samba4_install.sh
+++ samba4_install.sh
@@ -6,11 +6,11 @@
 sudo apt install acl attr autoconf bison build-essential debhelper dnsutils docbook-xml docbook-xsl flex gdb krb5-user libacl1-dev libaio-dev libattr1-dev libblkid-dev libbsd-dev libcap-dev libcups2-dev libgnutls28-dev libjson-perl libldap2-dev libncurses5-dev libpam0g-dev libparse-yapp-perl libpopt-dev libreadline-dev perl perl-modules-5.24 pkg-config python-all-dev python-dev python-dnspython python-crypto xsltproc zlib1g-dev libgpgme-dev python-gpgme python-m2crypto
 
 # Available link in https://www.samba.org/samba/download/
-wget https://download.samba.org/pub/samba/stable/samba-4.7.5.tar.gz
+wget https://download.samba.org/pub/samba/stable/samba-4.8.0.tar.gz
 
-tar -zxvf samba-4.7.5.tar.gz
+tar -zxvf samba-4.8.0.tar.gz
 
-cd samba-4.7.5
+cd samba-4.8.0
 
 #Verificar XATTR
 #cat /boot/config-4.9.0-3-amd64 | grep _ACL
@@ -22,7 +22,7 @@ make
 make install
 
 # Export environment variables
-echo '# Samba 4.7' > /etc/profile.d/samba4.sh
+echo '# Samba 4.8' > /etc/profile.d/samba4.sh
 echo 'export SAMBA_HOME="/opt/samba"' >> /etc/profile.d/samba4.sh
 echo 'export PATH="${PATH}:${SAMBA_HOME}/bin:${SAMBA_HOME}/sbin"' >> /etc/profile.d/samba4.sh
 # Read in .bashrc
