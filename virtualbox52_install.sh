d3291cdbf8688a887bdc5af8a180ef231825fb13
Rodrigo Fioravante <kbsafioravante@gmail.com>
2018-03-16 14:10:34 -0400

    Updated extesion-pack 5.2.6 link

--- virtualbox52_install.sh
+++ virtualbox52_install.sh
@@ -18,7 +18,7 @@ echo "deb http://download.virtualbox.org/virtualbox/debian $distro_codename cont
 apt update
 apt install -y virtualbox-5.2
 VBoxManage extpack uninstall "Oracle VM VirtualBox Extension Pack"
-wget -c https://download.virtualbox.org/virtualbox/5.2.6/Oracle_VM_VirtualBox_Extension_Pack-5.2.6-120293.vbox-extpack
-VBoxManage extpack install Oracle_VM_VirtualBox_Extension_Pack-5.2.6-120293.vbox-extpack --accept-license=56be48f923303c8cababb0bb4c478284b688ed23f16d775d729b89a2e8e5f9eb
-rm Oracle_VM_VirtualBox_Extension_Pack-5.2.6-120293.vbox-extpack
+wget -c https://download.virtualbox.org/virtualbox/5.2.6/Oracle_VM_VirtualBox_Extension_Pack-5.2.6.vbox-extpack
+VBoxManage extpack install Oracle_VM_VirtualBox_Extension_Pack-5.2.6.vbox-extpack --accept-license=56be48f923303c8cababb0bb4c478284b688ed23f16d775d729b89a2e8e5f9eb
+rm Oracle_VM_VirtualBox_Extension_Pack-5.2.6.vbox-extpack
 echo "### Fim da instalação do VirtualBox 5.2 ###"
