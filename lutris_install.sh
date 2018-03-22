0c5a3e72b5d92e51fd55592ffc14d4fd8d040df6
Rodrigo Fioravante <kbsafioravante@gmail.com>
2018-03-22 13:38:16 -0400

    Lutris Open Gaming Platform adding repo and installing

---
+++ lutris_install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Autor: Rodrigo Fioravante
+# Email: kbsafioravante@gmail.com
+
+# Lutris installation
+
+# Getting distro release
+ver=$(lsb_release -sr)
+
+if [ $ver != "17.10" -a $ver != "17.04" -a $ver != "16.04" ]; then
+  ver=16.04
+fi
+
+echo "deb http://download.opensuse.org/repositories/home:/strycore/xUbuntu_$ver/ ./" > /etc/apt/sources.list.d/lutris.list
+
+wget -q http://download.opensuse.org/repositories/home:/strycore/xUbuntu_$ver/Release.key -O- | apt-key add -
+
+apt update
+
+apt install lutris
