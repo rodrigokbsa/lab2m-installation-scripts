073edf2db6b24460b772d6076f3a48c1520cdd72
Rodrigo Fioravante <kbsafioravante@gmail.com>
2018-04-03 11:11:23 -0400

    Updated extesion-pack 5.2.8 link

--- virtualbox52_install.sh
+++ virtualbox52_install.sh
@@ -10,7 +10,7 @@ if [ "$distro" = "LinuxMint" ]; then
 else
   distro_codename="$(lsb_release --codename --short)"
 fi
-
+apt autoremove virtualbox*
 apt install dkms
 echo "### Iniciando instalação do VirtualBox 5.2 ###"
 wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
@@ -18,7 +18,7 @@ echo "deb http://download.virtualbox.org/virtualbox/debian $distro_codename cont
 apt update
 apt install -y virtualbox-5.2
 VBoxManage extpack uninstall "Oracle VM VirtualBox Extension Pack"
-wget -c https://download.virtualbox.org/virtualbox/5.2.6/Oracle_VM_VirtualBox_Extension_Pack-5.2.6.vbox-extpack
-VBoxManage extpack install Oracle_VM_VirtualBox_Extension_Pack-5.2.6.vbox-extpack --accept-license=56be48f923303c8cababb0bb4c478284b688ed23f16d775d729b89a2e8e5f9eb
-rm Oracle_VM_VirtualBox_Extension_Pack-5.2.6.vbox-extpack
+wget -c https://download.virtualbox.org/virtualbox/5.2.8/Oracle_VM_VirtualBox_Extension_Pack-5.2.8.vbox-extpack
+VBoxManage extpack install Oracle_VM_VirtualBox_Extension_Pack-5.2.8.vbox-extpack --accept-license=56be48f923303c8cababb0bb4c478284b688ed23f16d775d729b89a2e8e5f9eb
+rm Oracle_VM_VirtualBox_Extension_Pack-5.2.8.vbox-extpack
 echo "### Fim da instalação do VirtualBox 5.2 ###"
