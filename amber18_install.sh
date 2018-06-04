765e750f84063ed7bc56fef224962a3d0ffd6e8f
Rodrigo Fioravante <kbsafioravante@gmail.com>
2018-06-04 08:54:57 -0400

    Amber18 installation - not tested yet

---
+++ amber18_install.sh
@@ -0,0 +1,78 @@
+#!/bin/sh
+# Author: Rodrigo Fioravante
+# Email: kbsafioravante@gmail.com
+# Not tested
+
+### Amber18 (optional) + AmberTools18 ###
+echo "### Iniciando instalação do AmberTools18 + Amber18 ###"
+
+# Set true in whatever you need to install
+amber="true"
+mpi="true"
+cuda="true"
+cuda_version="cuda9"
+gpus=2
+
+place=`pwd`
+mpi_cores=$(grep -c ^processor /proc/cpuinfo)
+
+apt install -y bc csh flex gfortran g++ xorg-dev zlib1g-dev libbz2-dev patch openmpi-bin libopenmpi-dev
+
+# Download AmberTools18 and/or Amber18 (or put this files in the same folder than this script)
+if [ "$amber" = "true" ]; then
+  #wget Amber18.tar.bz2 # Get Amber18
+  #wget AmberTools18.tar.bz2 Get AmberTools18
+  tar xvfj AmberTools18.tar.bz2 -C /opt/
+  tar xvfj Amber18.tgz -C /opt/
+else
+  #wget AmberTools18.tar.bz2 Get AmberTools18
+  tar xvfj AmberTools18.tar.bz2 -C /opt/
+fi
+
+# Gnu - This one you ever need to compile!
+export AMBERHOME=/opt/Amber18
+cd $AMBERHOME
+yes | ./configure gnu
+source $AMBERHOME/amber.sh
+make install
+
+cp amber.sh /etc/profile.d/amber18.sh
+
+if [ "$mpi" = "true" ]; then
+  yes | ./configure -mpi gnu
+  export DO_PARALLEL="mpirun -np $mpi_cores"
+  echo "export DO_PARALLEL=\"mpirun -np $mpi_cores\"" >> amber.sh
+  make install
+fi
+
+if [ "$cuda" = "true" ]; then
+  sh $cuda_version\_install.sh
+
+  source /etc/profile.d/$cuda_version.sh
+
+  export CUDA_VISIBLE_DEVICES=0
+  yes | ./configure -cuda gnu
+  make install
+
+  if [ "$gpus" -gt 1 ]; then
+    export DO_PARALLEL="mpirun -np $gpus"
+    export CUDA_VISIBLE_DEVICES=0,1
+    yes | ./configure -cuda -mpi gnu
+    make install
+  fi
+fi
+
+## Amber Tests ##
+#make test.serial
+#make test.parallel
+#make test.cuda
+#make test.cuda_parallel
+#################
+
+cd $place
+rm Amber*
+
+echo "Para que as váriáveis do Amber funcionem corretamente para todos os \
+usuários é necessário reiniciar o computador."
+echo "### Fim da instalação do AmberTools17 e Amber16 ###"
+####################
