60981d125fee27691e20292ef249e6718073c05a
Rodrigo Fioravante <kbsafioravante@gmail.com>
2018-07-20 14:40:31 -0400

    Extension pack updated and fixed bugs

--- virtualbox52_install.sh
+++ virtualbox52_install.sh
@@ -1,25 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 # Author: Rodrigo Fioravante
 # Email: kbsafioravante@gmail.com
 
+vbox_version="5.2.16"
 distro="$(lsb_release --id --short)"
-vbox_version="5.2.12"
+distro_codename="$(lsb_release --codename --short)"
 
 if [ "$distro" = "LinuxMint" ]; then
   source /etc/os-release
-  distro_codename=$UBUNTU_CODENAME
-else
-  distro_codename="$(lsb_release --codename --short)"
+  distro_codename="$UBUNTU_CODENAME"
 fi
-apt autoremove virtualbox*
-apt install dkms
-echo "### Iniciando instalação do VirtualBox 5.2 ###"
+
+apt autoremove -y virtualbox*
+
+echo "### Starting VirtualBox 5.2 installation ###"
 wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
 echo "deb http://download.virtualbox.org/virtualbox/debian $distro_codename contrib" > /etc/apt/sources.list.d/virtualbox.list
 apt update
-apt install -y virtualbox-5.2
+apt install -y dkms virtualbox-5.2
 VBoxManage extpack uninstall "Oracle VM VirtualBox Extension Pack"
 wget -c https://download.virtualbox.org/virtualbox/$vbox_version/Oracle_VM_VirtualBox_Extension_Pack-$vbox_version.vbox-extpack
 VBoxManage extpack install Oracle_VM_VirtualBox_Extension_Pack-$vbox_version.vbox-extpack --accept-license=56be48f923303c8cababb0bb4c478284b688ed23f16d775d729b89a2e8e5f9eb
 rm Oracle_VM_VirtualBox_Extension_Pack-$vbox_version.vbox-extpack
-echo "### Fim da instalação do VirtualBox 5.2 ###"
+echo "### VirtualBox 5.2 installation finished ###"
