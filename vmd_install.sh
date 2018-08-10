196845b969f80f463d09f0daeec5cfb2c3a34b36
Rodrigo Fioravante <Rodrigo Fioravante>
2018-08-10 08:39:29 -0400

    Corrected some mistakes in vmd pearl configure script

--- vmd_install.sh
+++ vmd_install.sh
@@ -8,8 +8,8 @@ wget -c http://www.ks.uiuc.edu/Research/vmd/vmd-1.9.2/files/final/vmd-1.9.2.bin.
 tar -vzxf vmd-1.9.2.bin.LINUXAMD64-RHEL5.opengl.tar.gz
 
 cd vmd-1.9.2
-sed -i 's/$install_bin_dir=.*/$install_bin_dir="\/opt\/vmd\/bin"/' configure
-sed -i 's/$install_library_dir=.*/$install_library_dir="\/opt\/vmd\/lib"/' configure
+sed -i 's/$install_bin_dir=.*/$install_bin_dir="\/opt\/vmd\/bin";/' configure
+sed -i 's/$install_library_dir=.*/$install_library_dir="\/opt\/vmd\/lib";/' configure
 
 ./configure
 
@@ -20,3 +20,5 @@ make install
 echo '# VMD 1.9.2' > /etc/profile.d/vmd.sh
 echo 'export VMD_HOME="/opt/vmd"' >> /etc/profile.d/vmd.sh
 echo 'export PATH="${PATH}:${VMD_HOME}/bin"' >> /etc/profile.d/vmd.sh
+
+echo "To the environment works corretly in every user you need to restart this computer. Without rebooting you have to run 'source /etc/profile.d/vmd.sh' in each terminal session."
