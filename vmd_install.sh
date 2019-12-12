6a94882d995f35afb095ac3b43f5941e226a6cf2
Rodrigo Fioravante <Rodrigo Fioravante>
2019-12-12 16:33:35 -0400

    Updated to VMD 1.9.3 - Download still fail

--- vmd_install.sh
+++ vmd_install.sh
@@ -2,12 +2,12 @@
 # Autor: Rodrigo Fioravante
 # Email: kbsafioravante@gmail.com
 
-# VMD 1.9.2 installation
-wget -c http://www.ks.uiuc.edu/Research/vmd/vmd-1.9.2/files/final/vmd-1.9.2.bin.LINUXAMD64-RHEL5.opengl.tar.gz
+# VMD 1.9.3 installation
+wget -c 'https://www.ks.uiuc.edu/Research/vmd/vmd-1.9.3/files/final/vmd-1.9.3.bin.LINUXAMD64-CUDA8-OptiX4-OSPRay111p1.opengl.tar.gz'
 
-tar -vzxf vmd-1.9.2.bin.LINUXAMD64-RHEL5.opengl.tar.gz
+tar -vzxf "vmd-1.9.3.bin.LINUXAMD64-CUDA8-OptiX4-OSPRay111p1.opengl.tar.gz"
 
-cd vmd-1.9.2
+cd vmd-1.9.3
 sed -i 's/$install_bin_dir=.*/$install_bin_dir="\/opt\/vmd\/bin";/' configure
 sed -i 's/$install_library_dir=.*/$install_library_dir="\/opt\/vmd\/lib";/' configure
 
@@ -17,7 +17,7 @@ cd src
 make install
 
 # Export environment variables
-echo '# VMD 1.9.2' > /etc/profile.d/vmd.sh
+echo '# VMD 1.9.3' > /etc/profile.d/vmd.sh
 echo 'export VMD_HOME="/opt/vmd"' >> /etc/profile.d/vmd.sh
 echo 'export PATH="${PATH}:${VMD_HOME}/bin"' >> /etc/profile.d/vmd.sh
 
