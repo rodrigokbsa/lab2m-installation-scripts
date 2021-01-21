e6291b7cb4e17e6068d4815c4e657b7291d34ef3
Rodrigo Fioravante <kbsafioravante@gmail.com>
2021-01-21 11:25:39 -0400

    Removed some versions and added new ones. Fixing Linux Mint when getting Ubuntu based name.

--- cuda_install.sh
+++ cuda_install.sh
@@ -8,10 +8,10 @@ echo "### Starting Cuda installation ###"
 HOW_TO_USE="
 Installation of CUDA Toolkit - Use Nvidia Drivers provided by system.
 Debian 10: apt install nvidia-driver
-Ubuntu Bionic: apt install nvidia-driver-430 (recommended) (version: 390, 418 or 430 are available)
+Ubuntu Focal: apt install nvidia-driver-450 (recommended) (version: 390, 418, 430, 440, 450 and 455 are available)
 
 Usage: $0 [ -c | -h | -v ]
--i | --install-version    Here you can put the Cuda version you want to use. Available: 8.0, 9.0, 9.1, [9.2] -> default not required
+-i | --install-version    Here you can put the Cuda version you want to use. Available: 8.0, [9.2 - default], 10.2 and 11.1
 -h | --help               Show this message
 -v | --version            Show the program version
 
@@ -44,7 +44,7 @@ done
 distro="$(lsb_release --id --short)"
 distro_codename="$(lsb_release --codename --short)"
 
-if [ "$distro" = "LinuxMint" ]; then
+if [ "$distro" = "LinuxMint" -o "$distro" = "Linuxmint" ]; then
   source /etc/os-release
   distro_codename="$UBUNTU_CODENAME"
 fi
@@ -53,6 +53,33 @@ apt install -y build-essential linux-source linux-headers-$(uname -r) linux-imag
 #apt source linux-image-$(uname -r)
 
 case "$version" in
+  "11.1")
+    # Getting Cuda 11.1
+    wget https://developer.download.nvidia.com/compute/cuda/11.0.3/local_installers/cuda_11.0.3_450.51.06_linux.run
+
+    chmod +x cuda_11.0.3_450.51.06_linux.run
+
+    # Cuda 11.1 Toolkit
+    # You need to install the drivers via apt or driver manager.
+    ./cuda_11.0.3_450.51.06_linux.run --silent --toolkit --toolkitpath="/opt/cuda-$version" --no-opengl-libs --override
+
+  ;;
+  "10.2")
+    # Getting Cuda 10.2
+    wget http://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda_10.2.89_440.33.01_linux.run
+
+    # Getting Cuda 10.2 Updates (Patch 1 - August 26th 2020)
+    wget -c http://developer.download.nvidia.com/compute/cuda/10.2/Prod/patches/1/cuda_10.2.1_linux.run
+
+    chmod +x cuda_10.2.*
+
+    # Cuda 10.2 Toolkit
+    # You need to install the drivers via apt or driver manager.
+    ./cuda_10.2.89_440.33.01_linux.run --silent --toolkit --toolkitpath="/opt/cuda-$version" --no-opengl-libs --override
+
+    # Patch 1
+    ./cuda_10.2.1_linux.run --silent --accept-eula --installdir="/opt/cuda-$version"
+  ;;
   "9.2")
     # Getting Cuda 9.2
     wget -c https://developer.nvidia.com/compute/cuda/9.2/Prod2/local_installers/cuda_9.2.148_396.37_linux -O cuda_9.2.148_396.37_linux.run
@@ -62,62 +89,17 @@ case "$version" in
     chmod +x cuda_9.2.148*
 
     # Cuda 9.2 Toolkit
-    # You need to install the drivers via Driver Manager before it.
+    # You need to install the drivers via apt or driver manager.
     ./cuda_9.2.148_396.37_linux.run --silent --toolkit --toolkitpath="/opt/cuda-$version" --no-opengl-libs --override
 
     # Patch 1
     ./cuda_9.2.148.1_linux.run --silent --accept-eula --installdir="/opt/cuda-$version"
   ;;
-  "9.1")
-    # Getting Cuda 9.1
-    wget -c https://developer.nvidia.com/compute/cuda/9.1/Prod/local_installers/cuda_9.1.85_387.26_linux -O cuda_9.1.85_387.26_linux.run
-    # Getting Cuda 9.1 Updates (Patch 1, 2 and 3 - Mar 5th 2018)
-    wget -c https://developer.nvidia.com/compute/cuda/9.1/Prod/patches/1/cuda_9.1.85.1_linux -O cuda_9.1.85.1_linux.run
-    wget -c https://developer.nvidia.com/compute/cuda/9.1/Prod/patches/2/cuda_9.1.85.2_linux -O cuda_9.1.85.2_linux.run
-    wget -c https://developer.nvidia.com/compute/cuda/9.1/Prod/patches/3/cuda_9.1.85.3_linux -O cuda_9.1.85.3_linux.run
-
-    chmod +x cuda_9.1.85*
-
-    # Cuda 9.1 Toolkit
-    # You need to install the drivers via Driver Manager before it.
-    ./cuda_9.1.85_387.26_linux.run --silent --toolkit --toolkitpath="/opt/cuda-$version" --no-opengl-libs --override
-
-    # Patch 1
-    ./cuda_9.1.85.1_linux.run --silent --accept-eula --installdir="/opt/cuda-$version"
-    # Patch 2
-    ./cuda_9.1.85.2_linux.run --silent --accept-eula --installdir="/opt/cuda-$version"
-    # Patch 3
-    ./cuda_9.1.85.3_linux.run --silent --accept-eula --installdir="/opt/cuda-$version"
-  ;;
-  "9.0")
-    # Getting Cuda 9.0
-    wget -c https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda_9.0.176_384.81_linux-run -O cuda_9.0.176_384.81_linux.run
-    # Getting Cuda 9.0 Updates (Patch 1, 2, 3 and 4 - Aug 6th 2018)
-    wget -c https://developer.nvidia.com/compute/cuda/9.0/Prod/patches/1/cuda_9.0.176.1_linux-run -O cuda_9.0.176.1_linux.run
-    wget -c https://developer.nvidia.com/compute/cuda/9.0/Prod/patches/2/cuda_9.0.176.2_linux-run -O cuda_9.0.176.2_linux.run
-    wget -c https://developer.nvidia.com/compute/cuda/9.0/Prod/patches/3/cuda_9.0.176.3_linux-run -O cuda_9.0.176.3_linux.run
-    wget -c https://developer.nvidia.com/compute/cuda/9.0/Prod/patches/4/cuda_9.0.176.4_linux-run -O cuda_9.0.176.4_linux.run
-
-    chmod +x cuda_9.0.176*
-
-    # Cuda 9.0 Toolkit
-    # You need to install the drivers via Driver Manager before it.
-    ./cuda_9.0.176_384.81_linux.run --silent --toolkit --toolkitpath="/opt/cuda-$version" --no-opengl-libs --override
-
-    # Patch 1
-    ./cuda_9.0.176.1_linux.run --silent --accept-eula --installdir="/opt/cuda-$version"
-    # Patch 2
-    ./cuda_9.0.176.2_linux.run --silent --accept-eula --installdir="/opt/cuda-$version"
-    # Patch 3
-    ./cuda_9.0.176.3_linux.run --silent --accept-eula --installdir="/opt/cuda-$version"
-    # Patch 4
-    ./cuda_9.0.176.4_linux.run --silent --accept-eula --installdir="/opt/cuda-$version"
-  ;;
   "8.0")
   # Getting Cuda 8.0
   wget -c https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda_8.0.61_375.26_linux-run -O cuda_8.0.61_375.26_linux-run cuda_8.0.61_375.26_linux.run
   # Getting Cuda 8.0 Update (Patch 2 - Jun 26th 2017)
-  wget -c https://developer.nvidia.com/compute/cuda/8.0/Prod2/patches/2/cuda_8.0.61.2_linux-run -O cuda_8.0.61.2_linux-run -O cuda_8.0.61.2_linux.run
+  wget -c https://developer.nvidia.com/compute/cuda/8.0/Prod2/patches/2/cuda_8.0.61.2_linux-run -O cuda_8.0.61.2_linux-run
 
   chmod +x cuda_8.0.61*
 
@@ -129,7 +111,7 @@ case "$version" in
   fi
 
   # Cuda 8.0 Toolkit
-  # You need to install the drivers via Driver Manager before it.
+  # You need to install the drivers via apt or driver manager.
   ./cuda_8.0.61_375.26_linux.run --silent --toolkit --toolkitpath="/opt/cuda-$version" --override --no-opengl-libs --override
 
   # Patch 2
